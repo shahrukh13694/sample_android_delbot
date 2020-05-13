6fdd3821a381ec7240bfd280f6f5ea7793f7d2e9
sbhaidani <sbhaidani@deakin.edu.au>
2020-05-13 11:19:06 +1000

    Second Commit

--- app/src/main/java/com/example/delbot/MainActivity.java
+++ app/src/main/java/com/example/delbot/MainActivity.java
@@ -3,14 +3,27 @@ package com.example.delbot;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     ChatAdapter chatAdapter;
     List<ChatType> chatTypeList;
+
+    private WebSocketClient mWebSocketClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
         chatAdapter = new ChatAdapter(chatTypeList, this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
         recyclerView.setAdapter(chatAdapter);
+
+        connectWebSocket();
 
         inputText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             public void onFocusChange(View v, boolean hasFocus) {
@@ -61,5 +78,64 @@ public class MainActivity extends AppCompatActivity {
         int pos = layoutManager.findLastCompletelyVisibleItemPosition();
         int numItems = recyclerView.getAdapter().getItemCount();
         return (pos >= numItems);
+    }
+
+    private void connectWebSocket() {
+        URI uri;
+        try {
+            uri = new URI("ws://192.168.0.31:3051");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                String uniqueID = UUID.randomUUID().toString();
+                JSONObject messagePayload = new JSONObject();
+                try{
+                    messagePayload.put("type", "hello");
+                    messagePayload.put("User", uniqueID);
+                    messagePayload.put("text", "hi 2 from del-bot example android");
+                    messagePayload.put("channel", "socket");
+                    messagePayload.put("user_profile", null);
+                }
+                catch(JSONException ex) {
+                    ex.printStackTrace();
+                }
+                mWebSocketClient.send(messagePayload.toString());
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView textView = (TextView)findViewById(R.id.message);
+                        textView.setText(textView.getText() + "\n" + message);
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        mWebSocketClient.connect();
+    }
+
+    public void sendMessage(View view) {
+        EditText editText = (EditText)findViewById(R.id.message);
+        mWebSocketClient.send(editText.getText().toString());
+        editText.setText("");
     }
 }
